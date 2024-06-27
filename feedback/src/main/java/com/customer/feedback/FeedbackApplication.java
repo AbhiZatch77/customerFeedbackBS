16fa1b55cd11c161e97b0dcad5e87c8ba27c2f2d
Abhishek <abhishek200men@gmail.com>
2024-06-27 20:07:23 +0530

    test

--- feedback/src/main/java/com/customer/feedback/FeedbackApplication.java
+++ feedback/src/main/java/com/customer/feedback/FeedbackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeedbackApplication {
 
-	private String test;
+	private String test2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackApplication.class, args);
