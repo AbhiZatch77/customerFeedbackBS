92dad3353ba4f15b1fafc33bdec177e89199cb4b
Abhishek <abhishek200men@gmail.com>
2024-06-27 20:06:30 +0530

    test

---
+++ feedback/src/main/java/com/customer/feedback/FeedbackApplication.java
@@ -0,0 +1,15 @@
+package com.customer.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackApplication {
+
+	private String test;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackApplication.class, args);
+	}
+
+}
--- zshrc
+++
@@ -1,3 +0,0 @@
-export M2_HOME="/Users/user/Documents/apache-maven-3.9.6"
-PATH="${M2_HOME}/bin:${PATH}"
-export PATH
