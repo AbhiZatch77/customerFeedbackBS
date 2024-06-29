96d2b0236df1fab3c31e2aeec45046a8a24cf52c
Abhishek <abhishek200men@gmail.com>
2024-06-29 17:24:37 +0530

    Save feedback to h2

--- feedback/src/main/java/com/customer/feedback/FeedbackApplication.java
+++ feedback/src/main/java/com/customer/feedback/FeedbackApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeedbackApplication {
 
-	private String test2;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackApplication.class, args);
 	}
